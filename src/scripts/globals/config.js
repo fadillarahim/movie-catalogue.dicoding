2d372529cdf08b836209d6634e6b19ec0af2ff4a
fadillarahim <fadillarahimximipa1@gmail.com>
2022-10-02 20:06:23 +0700

    make a request to API MovieDB

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: 'c4097c9ba8379638440e3fae17fc2759',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
