8a64682ea068219d226863695ee546844ebc3132
fadillarahim <fadillarahimximipa1@gmail.com>
2022-10-03 10:32:26 +0700

    After implement Cache to service worker

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -1,21 +1,38 @@
 /* eslint-disable no-restricted-globals */
 /* eslint-disable no-unused-vars */
-self.addEventListener('install', (event) => {
-  console.log('Installing Service Worker ...');
+import 'regenerator-runtime';
+import CacheHelper from './utils/cache-helper';
 
-  // TODO: Caching App Shell Resource
+// Daftar Asset yang akan dicadangkan
+const assetsToCache = [
+  './',
+  './icons/maskable_icon.png',
+  './icons/maskable_icon_x48.png',
+  './icons/maskable_icon_x72.png',
+  './icons/maskable_icon_x96.png',
+  './icons/maskable_icon_x128.png',
+  './icons/maskable_icon_x192.png',
+  './icons/maskable_icon_x384.png',
+  './icons/maskable_icon_x512.png',
+  './index.html',
+  './favicon.png',
+  './app.bundle.js',
+  './app.webmanifest',
+  './sw.bundle.js',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(CacheHelper.cachingAppShell([...assetsToCache]));
 });
 
 self.addEventListener('activate', (event) => {
-  console.log('Activating Service Worker ...');
-
-  // TODO: Delete old caches
+  event.waitUntil(CacheHelper.deleteOldCache());
 });
 
 // eslint-disable-next-line no-restricted-globals
 self.addEventListener('fetch', (event) => {
   console.log(event.request);
 
-  event.respondWith(fetch(event.request));
+  event.respondWith(CacheHelper.revalidateChace(event.request));
   // TODO: Add/get fetch request to/from caches
 });
